6f20210c7c21dc031665b7bff28ff6bd1e1ab78b
Usuario <Usuario@CPD03-M12>
2024-08-20 12:20:56 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1_estados/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace WindowsFormsApp1_estados
+{
+    partial class v
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbTodos = new System.Windows.Forms.ComboBox();
+            this.cbNorte = new System.Windows.Forms.ComboBox();
+            this.cbnordeste = new System.Windows.Forms.ComboBox();
+            this.cbcentro = new System.Windows.Forms.ComboBox();
+            this.cbsudeste = new System.Windows.Forms.ComboBox();
+            this.cbsul = new System.Windows.Forms.ComboBox();
+            this.lbltodos = new System.Windows.Forms.Label();
+            this.lblnorte = new System.Windows.Forms.Label();
+            this.lblnordeste = new System.Windows.Forms.Label();
+            this.lblsudeste = new System.Windows.Forms.Label();
+            this.lblcentro = new System.Windows.Forms.Label();
+            this.lblsul = new System.Windows.Forms.Label();
+            this.bnttodos = new System.Windows.Forms.Button();
+            this.bntnorte = new System.Windows.Forms.Button();
+            this.bntnordeste = new System.Windows.Forms.Button();
+            this.bntcentro = new System.Windows.Forms.Button();
+            this.bntsudeste = new System.Windows.Forms.Button();
+            this.bntsul = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Todos os Estados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Região Norte";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Região Nortedeste";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Região Centro-oeste";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Região Sudeste";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Região sul";
+            // 
+            // cbTodos
+            // 
+            this.cbTodos.FormattingEnabled = true;
+            this.cbTodos.Items.AddRange(new object[] {
+            "Acre",
+            "Amapá",
+            "Amazonas",
+            "Maranhão",
+            "Mato Grosso",
+            "Pará",
+            "Paraná",
+            "Rondônia",
+            "Roraima",
+            "Tocantins",
+            "Bahia",
+            "Ceará",
+            "Maranhão",
+            "Paraíba",
+            "Pernambuco",
+            "Piaui",
+            "Rio Grande do Norte",
+            "Sergipe",
+            "Goiás",
+            "Mato Grosso",
+            "Mato Grosso do Sul",
+            "Distrito Federal",
+            "spírito Santo",
+            "Minas Gerais",
+            "Rio de Janeiro",
+            "São Paulo",
+            "Paraná",
+            "Rio Grande do Sul",
+            "Santa Catarina"});
+            this.cbTodos.Location = new System.Drawing.Point(146, 33);
+            this.cbTodos.Name = "cbTodos";
+            this.cbTodos.Size = new System.Drawing.Size(121, 21);
+            this.cbTodos.TabIndex = 6;
+            // 
+            // cbNorte
+            // 
+            this.cbNorte.FormattingEnabled = true;
+            this.cbNorte.Location = new System.Drawing.Point(146, 78);
+            this.cbNorte.Name = "cbNorte";
+            this.cbNorte.Size = new System.Drawing.Size(121, 21);
+            this.cbNorte.TabIndex = 7;
+            // 
+            // cbnordeste
+            // 
+            this.cbnordeste.FormattingEnabled = true;
+            this.cbnordeste.Location = new System.Drawing.Point(146, 130);
+            this.cbnordeste.Name = "cbnordeste";
+            this.cbnordeste.Size = new System.Drawing.Size(121, 21);
+            this.cbnordeste.TabIndex = 8;
+            // 
+            // cbcentro
+            // 
+            this.cbcentro.FormattingEnabled = true;
+            this.cbcentro.Location = new System.Drawing.Point(146, 179);
+            this.cbcentro.Name = "cbcentro";
+            this.cbcentro.Size = new System.Drawing.Size(121, 21);
+            this.cbcentro.TabIndex = 9;
+            // 
+            // cbsudeste
+            // 
+            this.cbsudeste.FormattingEnabled = true;
+            this.cbsudeste.Location = new System.Drawing.Point(146, 230);
+            this.cbsudeste.Name = "cbsudeste";
+            this.cbsudeste.Size = new System.Drawing.Size(121, 21);
+            this.cbsudeste.TabIndex = 10;
+            // 
+            // cbsul
+            // 
+            this.cbsul.FormattingEnabled = true;
+            this.cbsul.Location = new System.Drawing.Point(146, 283);
+            this.cbsul.Name = "cbsul";
+            this.cbsul.Size = new System.Drawing.Size(121, 21);
+            this.cbsul.TabIndex = 11;
+            // 
+            // lbltodos
+            // 
+            this.lbltodos.AutoSize = true;
+            this.lbltodos.Location = new System.Drawing.Point(409, 33);
+            this.lbltodos.Name = "lbltodos";
+            this.lbltodos.Size = new System.Drawing.Size(0, 13);
+            this.lbltodos.TabIndex = 12;
+            // 
+            // lblnorte
+            // 
+            this.lblnorte.AutoSize = true;
+            this.lblnorte.Location = new System.Drawing.Point(409, 86);
+            this.lblnorte.Name = "lblnorte";
+            this.lblnorte.Size = new System.Drawing.Size(0, 13);
+            this.lblnorte.TabIndex = 13;
+            // 
+            // lblnordeste
+            // 
+            this.lblnordeste.AutoSize = true;
+            this.lblnordeste.Location = new System.Drawing.Point(409, 138);
+            this.lblnordeste.Name = "lblnordeste";
+            this.lblnordeste.Size = new System.Drawing.Size(0, 13);
+            this.lblnordeste.TabIndex = 14;
+            // 
+            // lblsudeste
+            // 
+            this.lblsudeste.AutoSize = true;
+            this.lblsudeste.Location = new System.Drawing.Point(409, 233);
+            this.lblsudeste.Name = "lblsudeste";
+            this.lblsudeste.Size = new System.Drawing.Size(0, 13);
+            this.lblsudeste.TabIndex = 15;
+            // 
+            // lblcentro
+            // 
+            this.lblcentro.AutoSize = true;
+            this.lblcentro.Location = new System.Drawing.Point(409, 182);
+            this.lblcentro.Name = "lblcentro";
+            this.lblcentro.Size = new System.Drawing.Size(0, 13);
+            this.lblcentro.TabIndex = 16;
+            // 
+            // lblsul
+            // 
+            this.lblsul.AutoSize = true;
+            this.lblsul.Location = new System.Drawing.Point(409, 283);
+            this.lblsul.Name = "lblsul";
+            this.lblsul.Size = new System.Drawing.Size(0, 13);
+            this.lblsul.TabIndex = 17;
+            // 
+            // bnttodos
+            // 
+            this.bnttodos.Location = new System.Drawing.Point(296, 33);
+            this.bnttodos.Name = "bnttodos";
+            this.bnttodos.Size = new System.Drawing.Size(75, 23);
+            this.bnttodos.TabIndex = 18;
+            this.bnttodos.Text = "-----";
+            this.bnttodos.UseVisualStyleBackColor = true;
+            // 
+            // bntnorte
+            // 
+            this.bntnorte.Location = new System.Drawing.Point(296, 86);
+            this.bntnorte.Name = "bntnorte";
+            this.bntnorte.Size = new System.Drawing.Size(75, 23);
+            this.bntnorte.TabIndex = 19;
+            this.bntnorte.Text = "-----";
+            this.bntnorte.UseVisualStyleBackColor = true;
+            // 
+            // bntnordeste
+            // 
+            this.bntnordeste.Location = new System.Drawing.Point(296, 138);
+            this.bntnordeste.Name = "bntnordeste";
+            this.bntnordeste.Size = new System.Drawing.Size(75, 23);
+            this.bntnordeste.TabIndex = 20;
+            this.bntnordeste.Text = "-----";
+            this.bntnordeste.UseVisualStyleBackColor = true;
+            // 
+            // bntcentro
+            // 
+            this.bntcentro.Location = new System.Drawing.Point(296, 182);
+            this.bntcentro.Name = "bntcentro";
+            this.bntcentro.Size = new System.Drawing.Size(75, 23);
+            this.bntcentro.TabIndex = 21;
+            this.bntcentro.Text = "-----";
+            this.bntcentro.UseVisualStyleBackColor = true;
+            // 
+            // bntsudeste
+            // 
+            this.bntsudeste.Location = new System.Drawing.Point(296, 233);
+            this.bntsudeste.Name = "bntsudeste";
+            this.bntsudeste.Size = new System.Drawing.Size(75, 23);
+            this.bntsudeste.TabIndex = 22;
+            this.bntsudeste.Text = "-----";
+            this.bntsudeste.UseVisualStyleBackColor = true;
+            // 
+            // bntsul
+            // 
+            this.bntsul.Location = new System.Drawing.Point(296, 286);
+            this.bntsul.Name = "bntsul";
+            this.bntsul.Size = new System.Drawing.Size(75, 23);
+            this.bntsul.TabIndex = 23;
+            this.bntsul.Text = "-----";
+            this.bntsul.UseVisualStyleBackColor = true;
+            // 
+            // v
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 450);
+            this.Controls.Add(this.bntsul);
+            this.Controls.Add(this.bntsudeste);
+            this.Controls.Add(this.bntcentro);
+            this.Controls.Add(this.bntnordeste);
+            this.Controls.Add(this.bntnorte);
+            this.Controls.Add(this.bnttodos);
+            this.Controls.Add(this.lblsul);
+            this.Controls.Add(this.lblcentro);
+            this.Controls.Add(this.lblsudeste);
+            this.Controls.Add(this.lblnordeste);
+            this.Controls.Add(this.lblnorte);
+            this.Controls.Add(this.lbltodos);
+            this.Controls.Add(this.cbsul);
+            this.Controls.Add(this.cbsudeste);
+            this.Controls.Add(this.cbcentro);
+            this.Controls.Add(this.cbnordeste);
+            this.Controls.Add(this.cbNorte);
+            this.Controls.Add(this.cbTodos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "v";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.v_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbTodos;
+        private System.Windows.Forms.ComboBox cbNorte;
+        private System.Windows.Forms.ComboBox cbnordeste;
+        private System.Windows.Forms.ComboBox cbcentro;
+        private System.Windows.Forms.ComboBox cbsudeste;
+        private System.Windows.Forms.ComboBox cbsul;
+        private System.Windows.Forms.Label lbltodos;
+        private System.Windows.Forms.Label lblnorte;
+        private System.Windows.Forms.Label lblnordeste;
+        private System.Windows.Forms.Label lblsudeste;
+        private System.Windows.Forms.Label lblcentro;
+        private System.Windows.Forms.Label lblsul;
+        private System.Windows.Forms.Button bnttodos;
+        private System.Windows.Forms.Button bntnorte;
+        private System.Windows.Forms.Button bntnordeste;
+        private System.Windows.Forms.Button bntcentro;
+        private System.Windows.Forms.Button bntsudeste;
+        private System.Windows.Forms.Button bntsul;
+    }
+}
+
---
+++ WindowsFormsApp1_estados/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1_estados
+{
+    public partial class v : Form
+    {
+        public v()
+        {
+            InitializeComponent();
+        }
+
+        private void v_Load(object sender, EventArgs e)
+        {
+            cbTodos.Text.Count()this.label1.Text =
+            lbltodos.Text= cbTodos.Text.Count();
+        }
+    }
+}
